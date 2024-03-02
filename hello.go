4cd98a41c253d602fea91552caf018af60d672b0
Leon --Remote <leon@entwurf.co.uk>
2024-03-02 11:05:34 +0800

    refactored test and created fallback func for empty string

--- hello.go
+++ hello.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 
+const helloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+
+	return helloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello(""))
 }
